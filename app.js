a5101323d0d1f76b231992f8f4382a5dd9de9dea
Javan <javan510.gmail.com>
2021-05-24 18:55:18 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var count1 = 0;
+var count2 = 0;
+var count3 = 0;
+var element1 = document.querySelector("#count1");
+var element2 = document.querySelector("#count2");
+var element3 = document.querySelector("#count3");
+
+function likes1() {
+  count1++;
+
+  element1.innerText = count1 + " like(s)";
+  console.log(count);
+}
+function likes2() {
+  count2++;
+
+  element2.innerText = count2 + " like(s)";
+  console.log(count);
+}
+function likes3() {
+  count3++;
+
+  element3.innerText = count3 + " like(s)";
+  console.log(count);
+}
